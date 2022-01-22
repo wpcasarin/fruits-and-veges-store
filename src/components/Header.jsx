2ab1d42468b7247f13cbb9ad0198c9e555151e9b
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-01-22 15:24:57 -0300

    map pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,6 +19,34 @@ function Header({ currentPage }) {
     setValue(newValue);
   };
 
+  const pages = [
+    {
+      id: 0,
+      name: 'Início',
+      to: '/',
+    },
+    {
+      id: 1,
+      name: 'Produtos',
+      to: '/products',
+    },
+    {
+      id: 2,
+      name: 'Sobre Nós',
+      to: '/about',
+    },
+    {
+      id: 3,
+      name: '',
+      icon: (
+        <>
+          <CartIcon />
+        </>
+      ),
+      to: '/cart',
+    },
+  ];
+
   return (
     <AppBar position="static" sx={{ background: 'white' }}>
       <Container>
@@ -51,15 +79,15 @@ function Header({ currentPage }) {
             component={'nav'}
             sx={{ marginLeft: 'auto', flexGrow: 1 }}
           >
-            <Tab label="Início" component={NavLink} to="/" />
-            <Tab label="Produtos" component={NavLink} to="/products" />
-            <Tab label="Sobre Nós" component={NavLink} to="/about" />
-            <Tab
-              icon={<CartIcon />}
-              component={NavLink}
-              to="/cart"
-              sx={{ marginLeft: 'auto' }}
-            />
+            {pages.map((page, index) => (
+              <Tab
+                key={index}
+                label={page.name}
+                icon={page.icon}
+                component={NavLink}
+                to={page.to}
+              />
+            ))}
           </Tabs>
         </Toolbar>
       </Container>
